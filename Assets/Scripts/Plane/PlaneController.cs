1c31b3a50cdff785be32eaf010954aa6bbfa6d45
feyklave <rabia.kralice01@gmail.com>
2023-07-05 17:06:17 +0300

    airplane and level design

--- Assets/Scripts/Plane/PlaneController.cs
+++ Assets/Scripts/Plane/PlaneController.cs
@@ -4,9 +4,26 @@ using UnityEngine;
 
 public class PlaneController : MonoBehaviour
 {
-    public float speed;
 
-    // Update is called once per frame
+    public GameObject ucak;
+    public float speed;
+    public float lifespan = 5f;
+    private bool hasDestroyed = false;
+
+    void Start()
+    {
+        Invoke("YokEt", lifespan);
+    }
+
+    void YokEt()
+    {
+        if (!hasDestroyed)
+        {
+            hasDestroyed = true;
+            Destroy(ucak);
+        }
+    }
+
     void Update()
     {
         transform.position += transform.forward * Time.deltaTime * speed;
