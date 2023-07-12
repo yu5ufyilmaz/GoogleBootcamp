7dcafa7ff12f27a5e6aa4178410e3f28b3bb3a08
Yusuf Yilmaz <ysfyilmaz6@gmail.com>
2023-07-13 00:40:46 +0300

    Last Chapter First Part Created

---
+++ Assets/Scripts/TriggerorCollider/TriggerToFinal.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerToFinal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if ()
+        {
+            
+        }
+    }
+}
